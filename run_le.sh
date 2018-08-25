406097ca31481ec3682fb94124e315ed0521f177
huaijinwang <huaijinwang95@qq.com>
2018-08-26 00:46:47 +0800

    fixing run_script bug

--- run_le.sh
+++ run_le.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+iRRAM_HOME=/home/whj/iRRAM
+
 src_file=$1
 
-cmake-build-debug/my_le $src_file -rose:Cxx -I/home/whj/iRRAM/include
+cmake-build-debug/my_le $src_file -rose:Cxx -I$iRRAM_HOME/include -Itests/include
 cd klee_output
 ./make_expr.sh
 ./clean.sh
